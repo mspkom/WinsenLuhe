233aa8550fb84167e81b4a961a453da54387b40d
Manfred Specht <msp@area51.ms>
2017-10-14 16:11:01 +0200

    closed all eslint-errors

--- src/tools.js
+++ src/tools.js
@@ -1,102 +1,112 @@
 module.exports = {
-	isVocal: function(val) {
-		const vocals = ['A', 'Ä', 'E', 'I', 'O', 'Ö', 'U', 'Ü'];
+  isVocal(val) {
+    const vocals = ['A', 'Ä', 'E', 'I', 'O', 'Ö', 'U', 'Ü'];
 
-		return vocals.indexOf(val) === -1 ? false : true;
-	},
-	splitValue: function(val) {
-		return val.split('');
-	},
-	joinValue: function(val) {
-		return val.join('');
-	},
-	changeOne: function(startArr, val) {
-		let isVocal = this.isVocal(val);
+    return vocals.indexOf(val) >= 0;
+  },
+  splitValue: val => val.split(''),
+  joinValue: val => val.join(''),
+  changeOne(startArr, val) {
+    const isVocal = this.isVocal(val);
+    const newArray = startArr;
 
-		if (isVocal) {
-			startArr[1] = val;
-			startArr[9] = val;
-		} else {
-			startArr[0] = val;
-			startArr[8] = val;
-		}
+    if (isVocal) {
+      newArray[1] = val;
+      newArray[9] = val;
+    } else {
+      newArray[0] = val;
+      newArray[8] = val;
+    }
 
-    return startArr;
-	},
-	changeTwo: function(startArr, valArr) {
-		valArr.map((val, i) => {
-			let isVocal = this.isVocal(val);
+    return newArray;
+  },
+  changeTwo(startArr, valArr) {
+    const newArray = startArr;
 
-			if (i === 0) {
-				if (isVocal) { startArr[1] = val } else { startArr[0] = val }
-			}
+    valArr.map((val, i) => {
+      const isVocal = this.isVocal(val);
 
-			if (i === 1) {
-				if (isVocal) { startArr[9] = val } else { startArr[8] = val }
-			}
+      if (i === 0) {
+        if (isVocal) {
+          newArray[1] = val;
+        } else {
+          newArray[0] = val;
+        }
+      }
 
-    return startArr;
-		});
-	},
-	changeThree: function(startArr, valArr) {
-		let first = false;
+      if (i === 1) {
+        if (isVocal) {
+          newArray[9] = val;
+        } else {
+          newArray[8] = val;
+        }
+      }
 
-		valArr.map((val, i) => {
-			let isVocal = this.isVocal(val);
+      return newArray;
+    });
+  },
+  changeThree(startArr, valArr) {
+    const newArray = startArr;
+    let first = false;
 
-			if (i === 0) {
-				if (isVocal) {
-					startArr[1] = val;
-					first = true;
-				} else {
-					startArr[0] = val;
-				}
-			}
+    valArr.map((val, i) => {
+      const isVocal = this.isVocal(val);
 
-			if (i === 1) {
-				if (isVocal && !first) {
-					startArr[1] = val;
-				}
-				if (isVocal && first) {
-					startArr[4] = val;
-				}
-				if (!isVocal && !first) {
-					startArr[2] = val;
-				}
-				if (!isVocal && first) {
-					startArr[2] = val;
-				}
-			}
+      if (i === 0) {
+        if (isVocal) {
+          newArray[1] = val;
+          first = true;
+        } else {
+          newArray[0] = val;
+        }
+      }
 
-			if (i === 2) {
-				if (isVocal) { startArr[9] = val } else { startArr[8] = val }
-			}
+      if (i === 1) {
+        if (isVocal && !first) {
+          newArray[1] = val;
+        }
+        if (isVocal && first) {
+          newArray[4] = val;
+        }
+        if (!isVocal && !first) {
+          newArray[2] = val;
+        }
+        if (!isVocal && first) {
+          newArray[2] = val;
+        }
+      }
 
-      return startArr;
-		});
-	},
-	getStop: function(start, val) {
-		let startArr = this.splitValue(start);
-		const valArr = this.splitValue(val);
+      if (i === 2) {
+        if (isVocal) {
+          newArray[9] = val;
+        } else {
+          newArray[8] = val;
+        }
+      }
 
-		if (val.length === 1) {
-			this.changeOne(startArr, val);
-		}
+      return newArray;
+    });
+  },
+  getStop(start, val) {
+    const startArr = this.splitValue(start);
+    const valArr = this.splitValue(val);
 
-		if (val.length === 2) {
-			this.changeTwo(startArr, valArr);
-		}
+    if (val.length === 1) {
+      this.changeOne(startArr, val);
+    }
 
-		if (val.length === 3) {
-			this.changeThree(startArr, valArr);
-		}
+    if (val.length === 2) {
+      this.changeTwo(startArr, valArr);
+    }
 
-		if (val.length > 3) {
-			return 'Eingabe zu lang.';
-		}
+    if (val.length === 3) {
+      this.changeThree(startArr, valArr);
+    }
 
-		start = this.joinValue(startArr);
+    if (val.length > 3) {
+      return 'Eingabe zu lang.';
+    }
 
-		return start;
-	}
+    return this.joinValue(startArr);
+  },
 };
--- src/winsenLuhe.js
+++ src/winsenLuhe.js
@@ -4,16 +4,16 @@ console.log('WINSEN (LUHE)');
 const rl = require('readline');
 const tools = require('./tools');
 
-let prompts = rl.createInterface(process.stdin, process.stdout);
-let question = 'Nummernschild: ';
+const prompts = rl.createInterface(process.stdin, process.stdout);
+const question = 'Nummernschild: ';
 
-prompts.question(question, function(value) {
-	const val = value.toUpperCase();
-	const start = 'WINSEN (LUHE)';
-	const stop = tools.getStop(start, val);
+prompts.question(question, (value) => {
+  const val = value.toUpperCase();
+  const start = 'WINSEN (LUHE)';
+  const stop = tools.getStop(start, val);
 
-	console.log('RESULT =>', stop);
-	console.log('----------------------------------------');
+  console.log('RESULT =>', stop);
+  console.log('----------------------------------------');
 
-	process.exit();
+  process.exit();
 });
